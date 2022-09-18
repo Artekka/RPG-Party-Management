1447bdcf9c56d797bf5b441728ee75acaebe2bbe
Artekka <arthur.ohumukini@gmail.com>
2022-09-17 17:17:02 -0700

    Added local storage for selected party, unit list

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Units from './components/Units'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import unitList from "./assets/units/unitList"
 
 function App() {
 
-  const [selectedParty, setParty] = useState("Party 1");
+  const [selectedParty, setParty] = useState(JSON.parse(localStorage.getItem('selectedParty')) || "Party 1");
 
-  const [units, setUnits] = useState(unitList);
+  const [units, setUnits] = useState(JSON.parse(localStorage.getItem('unitList')) || unitList);
 
+  useEffect(() => {
+    localStorage.setItem('unitList', JSON.stringify(units));
+  }, [units]);
+
+  useEffect(() => {
+    localStorage.setItem('selectedParty', JSON.stringify(selectedParty));
+  }, [selectedParty]);
 
   function handlePartySelectionChange(e) {
       setParty(e.target.value);
